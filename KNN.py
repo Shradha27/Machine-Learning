7024bf5a8c9418fbfed270a417f109df8191301e
Shradha27 <shradha2704@gmail.com>
2018-02-16 21:59:08 +0530

    Plotting centroids

--- KNN.py
+++ KNN.py
@@ -3,13 +3,7 @@ from matplotlib import style
 from sklearn.cluster import KMeans
 import numpy as np
 
-X = np.array([[1,2],
-              [2,3],
-              [12,12],
-              [8,9],
-              [10,11],
-              [10,10],
-              [1,3]])
+X = np.array([[1, 2], [2, 3], [12, 12], [8, 9], [10, 11], [10, 10], [1, 3]])
 
 clf = KMeans(n_clusters=2)
 clf.fit(X)
@@ -18,14 +12,14 @@ centroids = clf.cluster_centers_
 labels = clf.labels_
 
 for i in range(len(centroids)):
-    print("centroids:",centroids[i][0],centroids[i][1])
+    print("centroids:", centroids[i][0], centroids[i][1])
 
 for i in range(len(labels)):
     print(labels[i])
 
-colors = ["r.","b.","k.","g.","c."]
+colors = ["r.", "b.", "k.", "g.", "c."]
 for i in range(len(X)):
-    plt.plot(X[i][0],X[i][1],colors[labels[i]], markersize = 15)
-plt.scatter(centroids[:,0],centroids[:,1],marker = 'x',s=100,linewidths=5)
+    plt.plot(X[i][0],X[i][1], colors[labels[i]], markersize = 15)
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=100, linewidths=5)
 plt._show()
 
