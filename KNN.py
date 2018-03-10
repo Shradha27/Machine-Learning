1585dc526d7e250868f44cca598986e851403414
Shradha27 <shradha2704@gmail.com>
2018-03-10 22:29:19 +0530

    KNN

--- KNN.py
+++ KNN.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import svm
+from sklearn import neighbors
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
@@ -16,7 +16,8 @@ y = np.array(df['class'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-clf = svm.SVC()
+# Default k = 5
+clf = neighbors.KNeighborsClassifier()
 # train the model on training data
 clf.fit(X_train, y_train)
 
