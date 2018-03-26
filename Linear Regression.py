915dec11453f5891780fcd67a644e0b746687664
Shradha27 <shradha2704@gmail.com>
2018-03-26 23:48:24 +0530

    Updated linear regression

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import quandl
 import math
+
+
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 df['LH_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low']
