7fe553d9e6d9915f772b452526b9bf34144906dc
Shradha27 <shradha2704@gmail.com>
2018-04-08 23:30:01 +0530

    Updated KNN

--- KNN.py
+++ KNN.py
@@ -1,32 +1,61 @@
 import numpy as np
-from sklearn import neighbors
-from sklearn.model_selection import train_test_split
+from collections import Counter
+import warnings
+import random
 import pandas as pd
 
-# Load the data set
-df = pd.read_csv('Datasets/breast_cancer_wisconsin_data.txt')
 
-# Replace the unassigned values with -99999 and drop the id column
+def k_nearest_neighbors(data, predict, k=5):
+    if k > sum(len(v) for v in data.values()):
+        warnings.warn('K is set to a value more than total data points!')
+    distances = []
+
+    for label in data:
+        for features in data[label]:
+            euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
+
+            distances.append([euclidean_distance, label])
+
+    votes = [i[1] for i in sorted(distances)[:k]]
+    vote_results = Counter(votes).most_common(1)[0][0]
+
+    confidence = Counter(votes).most_common(1)[0][1] / k
+    return vote_results, confidence
+
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+
 df.replace('?', -99999, inplace=True)
 df.drop(['id'], 1, inplace=True)
 
-# Get the features and labels
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+full_data = df.astype(float).values.tolist()
+random.shuffle(full_data)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+train_set = {2: [], 4: []}
+test_set = {2: [], 4: []}
 
-# Default k = 5
-clf = neighbors.KNeighborsClassifier()
-# train the model on training data
-clf.fit(X_train, y_train)
+test_size = 0.2
+train_data = full_data[:-int(test_size * len(full_data))]
+test_data = full_data[-int(test_size * len(full_data)):]
 
-# Get the accuracy on the testing data
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
 
-# Make predictions on some random values
-example_dataset = np.array([[8,3,3,1,2,3,4,4,2], [3,3,2,3,1,3,5,4,1], [5,3,3,1,2,2,4,3,1]])
-# example_dataset = example_dataset.reshape(len(example_dataset), -1)
-prediction = clf.predict(example_dataset)
-print(prediction)
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for label in test_set:
+    for predict in test_set[label]:
+        vote, confidence = k_nearest_neighbors(train_set, predict, k=5)
+
+        if vote == label:
+            correct += 1
+        else:
+            print(confidence)
+
+        total += 1
+
+print('Accuracy:', correct/total)
